a1f936e222195e5d1e25abcb90d3ba9518c5a38a
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-09-26 15:13:57 -0300

    Config and Create Table

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -1,12 +1,17 @@
+package config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class DatabaseConnection {
-		private static final String DB_NAME = System.getenv("DB_DATABASE");
+		private static final Dotenv dotenv = Dotenv.load();
+
+		private static final String DB_NAME = dotenv.get("DB_DATABASE");
     private static final String URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
     private static final String USER = "root"; // Substitua pelo seu usuário
-    private static final String PASSWORD = System.getenv("DB_ROOT_PASSWORD"); // Substitua pela sua senha
+    private static final String PASSWORD = dotenv.get("DB_ROOT_PASSWORD"); // Substitua pela sua senha
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
---
+++ src/dao/ProductDAO.java
@@ -0,0 +1,11 @@
+package dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import config.DatabaseConnection;
+import entity.Product;
+
+public class ProductDAO {
+
+}
